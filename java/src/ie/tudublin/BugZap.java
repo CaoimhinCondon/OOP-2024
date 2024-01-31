ebcd3717002b1024e0569c3519984188c6763353
Caomhin Condon <caoimhinecondon@gmail.com>
2024-01-31 11:53:30 +0000

    lab 2 Practice

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,66 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet{
+
+    float playerX;
+    float playerY;
+    float playerWidth = 30;
+    float movementSpeed = 2;
+
+    public void settings(){
+
+        size(500, 500);
+    }
+
+    public void setup(){
+
+        playerX = width/2;
+        playerY = height - 20;
+
+        background(0);
+        drawPlayer(playerX, playerY, playerWidth);
+    }
+
+    public void drawPlayer(float playerX, float playerY, float playerWidth){
+
+        float playerHeight = playerWidth/2;
+
+        //Set outline colour to white
+        stroke(255);
+        strokeWeight(2);
+
+        //Set fill to black
+        fill(0);
+
+        // Calculate the top-left corner of the rectangle
+        float rectX = playerX - playerWidth / 2;
+        float rectY = playerY - playerHeight / 2;
+
+        // Draw the rectangle
+        rect(rectX, rectY, playerWidth, playerHeight);
+    }
+
+    public void playerInput(){
+
+        if (keyCode == RIGHT && keyPressed == true){
+
+            background(0);
+            playerX += movementSpeed;
+            drawPlayer(playerX, playerY, playerWidth);
+        }
+        
+        if (keyCode == LEFT && keyPressed == true){
+
+            background(0);
+            playerX -= movementSpeed;
+            drawPlayer(playerX, playerY, playerWidth);
+        }
+    }
+
+    public void draw(){
+
+        playerInput();
+    }
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -4,26 +4,10 @@ package ie.tudublin;
 
 public class Main{
 
-	public void MyFirstProcessing(){
+	public void BugZap(){
 
 		String[] a = {"MAIN"};
-		processing.core.PApplet.runSketch(a, new MyFirstProcessing());
-	}
-
-	public void HelloProcessing(){
-
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch(a, new HelloProcessing());
-    }
-
-	public void doDogStuff(){
-		// Create a new Dog called Misty
-		// Call the constructor
-		Dog misty = new Dog("Misty");
-		misty.speak();
-
-        Dog lucy = new Dog("Lucy");
-		lucy.speak();
+		processing.core.PApplet.runSketch(a, new BugZap());
 	}
 	
 	public static void main(String[] args){
@@ -32,29 +16,6 @@ public class Main{
 		
 		Main m = new Main();
 
-		m.doDogStuff();
-
-		Dog nuala = new Dog("Nuala");
-		nuala.speak();
-
-		Cat atuma = new Cat("Atuma");
-		atuma.speak();
-
-		Animal larry = new Dog("Larry");
-		larry.speak();
-		larry = new Cat("TopCat");
-		larry.speak();
-
-		Animal a = new Dog("A");
-		//Animal b = new Dog("B");
-		Animal c = a;
-
-		a.setName("G");
-
-		System.out.println(a.getName());
-		System.out.println(c.getName());
-
-		//m.MyFirstProcessing();
-		m.HelloProcessing();
+		m.BugZap();
 	}
 }
